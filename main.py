7fb7d0c59a8125f6af0eb390bc8842559482402a
Tae-Hwan Kim <sky080895@naver.com>
2021-03-07 17:56:03 -0600

    Second Commit

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from tkinter import *
 from tkinter import font
 import pytube
 import buttonMethod as bm
+import imageio
 from PIL import Image, ImageTk
+import os
+import time
+from threading import *
 
 # from PIL import ImageTk, Image
 # from pathlib import Path
@@ -11,6 +15,7 @@ from PIL import Image, ImageTk
 # import pygame
 
 urlList = []
+musicList = []
 
 ################################
 # Call methods from buttonMethod
@@ -41,8 +46,7 @@ urlEntry.place(x=800,y=40)
 
 
 
-videoLabel = Label(win)
-videoLabel.pack()
+
 
 
 # def urlType():
@@ -63,19 +67,23 @@ urlLabel.pack()
 urlLabel.place(x=800, y=11)
 
 #Music List
-musicList = Text(win, height = 22, width = 40)
-musicList.pack()
-musicList.place(x=800, y = 170)
+playList = Text(win, height = 22, width = 40)
+playList.pack()
+playList.place(x=800, y = 170)
 
 ########### buttons ############
 
 def urlType():
     url = urlEntry.get()  # a is the thing typed in the entry
+    urlList.append(url)
     youtube = pytube.YouTube(url)
-    video = youtube.streams.first()
-    video.download()
-    name = youtube.streams[0].title
-    musicList.insert(END,name +"\n")
+    videoName = youtube.streams[0].title
+    newName = videoName.replace('/','')
+    musicList.append(newName)
+    playList.delete('1.0', END)
+    for music in musicList:
+        playList.insert(END,music +"\n")
+    print(musicList)
 
 #예약
 reserve = Button(win, text='예약', font = fixedSys25)  # construct a button
@@ -84,14 +92,57 @@ reserve.place(x=800, y=90)
 reserve.config(width=8, height=1)  # button size
 reserve.config(command= urlType)  # execute the function of reserve
 
-
+def urlTypeAhead():
+    url = urlEntry.get()  # a is the thing typed in the entry
+    urlList.insert(0, url)
+    youtube = pytube.YouTube(url)
+    videoName = youtube.streams[0].title
+    newName = videoName.replace('/','')
+    musicList.insert(0, newName)
+    playList.delete('1.0', END)
+    for music in musicList:
+        playList.insert(END,music +"\n")
+    print(musicList)
 
 #우선예약
 reserveAhead = Button(win, text='우선예약', font = fixedSys25)  # construct a button
 reserveAhead.pack()  # put a button on the window
 reserveAhead.place(x=1000, y=90)
 reserveAhead.config(width=8, height=1)  # button size
-#reserve.config(command=urlType)  # execute the function of button
+reserveAhead.config(command=urlTypeAhead)  # execute the function of button
+
+
+
+
+def screenGenerator(label):
+    youtube = pytube.YouTube(urlList[0])
+    video = youtube.streams.first()
+    video.download()
+    videoname = musicList[0]+'.mp4'
+    downloadedVideo = imageio.get_reader(videoname)
+
+
+    for image in downloadedVideo.iter_data():
+        frame_image = ImageTk.PhotoImage(Image.fromarray(image))
+        label.config(image=frame_image)
+        label.image = frame_image
+        time.sleep(0.025)
+
+
+
+def startVideo():
+    videoLabel = Label(win)
+    videoLabel.pack()
+    videoLabel.place(x=50, y=50)
+    thread = Thread(target=screenGenerator,args=(videoLabel,))
+    thread.daemon = 1
+    thread.start()
+
+    playList.delete('1.0', END)
+    for i in range(1,len(musicList)):
+        playList.insert(END, musicList[i] + "\n")
+
+    # if thread.is_alive():
 
 
 # start
@@ -99,49 +150,42 @@ start = Button(win,font = fixedSys25, text='시작')  # construct a button
 start.pack()  # put a button on the window
 start.place(x=800, y=600)
 start.config(width=8, height=1)  # button size
-#reserve.config(command=startVideo)  # execute the function of button # # # video = imageio.get_reader(video_name)
-
-# def startVideo(label):
+start.config(command=startVideo)
 #
-#     for image in video.iter_data():
-#         frame_image = ImageTk.PhotoImage(Image.fromarray(image))
-#         label.config(image=frame_image)
-#         label.image = frame_image
-#         time.sleep(0.025)
+def cancelMusic():
+    if thread.is_alive():
+        running_event.clear()
+        thread.join()
 
-
-
-# delay = int(1000 / video.get_meta_data()['fps'])
-#
-# def stream(label):
-#     try:
-#         image = video.get_next_data()
-#     except:
-#         video.close()
-#         return
-#     label.after(delay, lambda: stream(label))
-#     frame_image = ImageTk.PhotoImage(Image.fromarray(image))
-#     label.config(image=frame_image)
-#     label.image = frame_image
-#
-# videoLabel.after(delay,lambda: stream(videoLabel) )
-
-
-
+    del(urlList[0])
+    del(musicList[0])
+    playList.delete('1.0', END)
+    for music in musicList:
+        playList.insert(END,music +"\n")
+    print(musicList)
 
 #cancel
 cancel = Button(win, text='취소', font = fixedSys25)  # construct a button
 cancel.pack()  # put a button on the window
 cancel.place(x=1000, y=600)
 cancel.config(width=8, height=1)  # button size
-#reserve.config(command=urlType)  # execute the function of button
+# cancel.config(command=cancelMusic)  # execute the function of button
 
-#cancel reserve
+def cancelReservation():
+    del(urlList[0])
+    del(musicList[0])
+    playList.delete('1.0', END)
+    for music in musicList:
+        playList.insert(END,music +"\n")
+
+#cancel reservation
 cancelReserve = Button(win, text='예약취소', font = fixedSys25)  # construct a button
 cancelReserve.pack()  # put a button on the window
 cancelReserve.place(x=1000, y=680)
 cancelReserve.config(width=8, height=1)  # button size
-#reserve.config(command=urlType)  # execute the function of button
+cancelReserve.config(command=cancelReservation)  # execute the function of button
+
+
 
 # # pause
 pause= Button(win,font = fixedSys25, text='일시정지')  # construct a button
@@ -212,7 +256,8 @@ spdDown.config(width=8, height=1)  # button size
 win.mainloop()  # Execute the window
 
 
-# import tkinter as tk, threading
+# import tkinter as tk
+# from threading import *
 # import imageio
 # import os
 # import time
@@ -220,38 +265,22 @@ win.mainloop()  # Execute the window
 #
 # print (os.getcwd())
 #
-# video_name = "[TJ노래방] 사랑이었나봐 - 기리보이  TJ Karaoke"+".mp4" #This is your video file path
+# video_name = "[TJ노래방] 사랑이었나봐 - 기리보이  TJ Karaoke.mp" #This is your video file path
 # video = imageio.get_reader(video_name)
 #
-
+# def stream(label):
 #
+#     for image in video.iter_data():
+#         frame_image = ImageTk.PhotoImage(Image.fromarray(image))
+#         label.config(image=frame_image)
+#         label.image = frame_image
 #
 # if __name__ == "__main__":
 #
 #     root = tk.Tk()
 #     my_label = tk.Label(root)
 #     my_label.pack()
-#     my_label.place(x=50, y=50)
-#     thread = threading.Thread(target=stream, args=(my_label,))
+#     thread = Thread(target=stream, args=(my_label,))
 #     thread.daemon = 1
 #     thread.start()
 #     root.mainloop()
-
-import numpy as np
-import cv2
-import time
-
-# cap = cv2.VideoCapture('[TJ노래방] 사랑이었나봐 - 기리보이  TJ Karaoke.mp4')
-# i=0 #frame counter
-# frameTime = 1 # time of each frame in ms, you can add logic to change this value.
-# while(cap.isOpened()):
-#     ret = cap.grab() #grab frame
-#     i=i+1 #increment counter
-#     if i % 3 == 0: # display only one third of the frames, you can change this parameter according to your needs
-#         ret, frame = cap.retrieve() #decode frame
-#         cv2.imshow('frame',frame)
-#         if cv2.waitKey(100) & 0xFF == ord('q'):
-#             break
-#     #time.sleep(24)
-# cap.release()
-# cv2.destroyAllWindows()
